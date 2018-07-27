f2aa61950d7eeb92ecf465917e44852f97988302
fabiomagalhaes890 <fabiomagalhaes890@gmail.com>
2018-07-27 09:19:46 -0300

    Projeto base para desenvolvimento REACT

---
+++ src/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React, { PureComponent } from 'react'
+
+class App extends PureComponent {
+
+  render () {
+    return (
+      <div>My App</div>
+    )
+  }
+}
+
+export default App
